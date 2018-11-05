2032dfce3ab084dc85742f1a6f1698af1cf3ec6b
Antonio <benincasaantonio98@gmail.com>
2018-11-05 09:32:31 +0100

    Now the user is able to search a location

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const request = require("request");
+const yargs = require("yargs");
 
+const argv = yargs.options({
+  address: {
+    demand: true,
+    alias: 'a',
+    describe: "Address to fetch weather for",
+    string: true
+  }
+}).help().alias('help', 'h').argv;
+
+
+let address = encodeURI(argv['a']);
 request(
   {
-    url: "https://www.mapquestapi.com/geocoding/v1/address?key=hO3jIfrrkAlVg7tdAd8XXVAmGYYzor6l&location=hollywood%20blvd%20los%20angeles",
+    url:
+      `https://www.mapquestapi.com/geocoding/v1/address?key=hO3jIfrrkAlVg7tdAd8XXVAmGYYzor6l&location=${address}`,
     json: true
   },
   (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    for(let location of body.results) {
+      console.log('street', locations.street);
+      console.log('street', locations.adminArea5);
+    }
   }
 );
