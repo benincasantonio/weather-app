0006a6621c5bfa73dabed79f07d439fd84109d46
Antonio <benincasaantonio98@gmail.com>
2018-11-05 09:42:42 +0100

    Fix result display error Updated the git ignore file

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ request(
   },
   (error, response, body) => {
     for(let location of body.results) {
-      console.log('street', locations.street);
-      console.log('street', locations.adminArea5);
+      console.log('street', location.locations[0].street);
+      console.log('street', location.locations[0].adminArea5);
     }
   }
 );
