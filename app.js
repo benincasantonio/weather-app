4b724d99199c96bbe4b0a4f8584109959e38c60a
Antonio <benincasaantonio98@gmail.com>
2018-11-05 17:02:36 +0100

    Now the search show the data correctly

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const yargs = require("yargs");
-const geocode = require('./geocode/geocode');
+const geocode = require("./geocode/geocode");
 
-const argv = yargs.options({
-  address: {
-    demand: true,
-    alias: 'a',
-    describe: "Address to fetch weather for",
-    string: true
+const argv = yargs
+  .options({
+    address: {
+      demand: true,
+      alias: "a",
+      describe: "Address to fetch weather for",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
+geocode.geocodeAddress(argv["a"], (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
   }
-}).help().alias('help', 'h').argv;
-
-geocode.geocodeAddress (argv['a']);
+});
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,21 +1,30 @@
 const request = require("request");
 
-function geocodeAddress(address) {
-    let addressURI = encodeURIComponent(address);
-request(
-  {
-    url:
-      `https://www.mapquestapi.com/geocoding/v1/address?key=hO3jIfrrkAlVg7tdAd8XXVAmGYYzor6l&location=${addressURI}`,
-    json: true
-  },
-  (error, response, body) => {
-    for(let location of body.results) {
-      console.log('street', location.locations[0].street);
-      console.log('street', location.locations[0].adminArea5);
+function geocodeAddress(address, callback) {
+  let addressURI = encodeURIComponent(address);
+  request(
+    {
+      url: `https://www.mapquestapi.com/geocoding/v1/address?key=hO3jIfrrkAlVg7tdAd8XXVAmGYYzor6l&location=${addressURI}`,
+      json: true
+    },
+    (error, response, body) => {
+      if (error) {
+        callback("Unable to reach the Server");
+      } else {
+        if (body.results.length > 0) {
+          for (let location of body.results) {
+            let address = {
+              address: location.locations[0].street,
+              geometry: location.locations[0].latLng
+            };
+            callback(undefined, address);
+          }
+        }else {
+            callback('No Result')
+        }
+      }
     }
-  }
-);
-
+  );
 }
 
 module.exports.geocodeAddress = geocodeAddress;
