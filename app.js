21b26c9737e9c4fe2d2c63decd92ef8228427939
Antonio <benincasaantonio98@gmail.com>
2018-11-19 09:10:23 +0100

    Now you can fetch the temperature

--- app.js
+++ app.js
@@ -17,23 +17,18 @@ geocode.geocodeAddress(argv["a"], (errorMessage, results) => {
   if (errorMessage) {
     console.log(errorMessage);
   } else {
-    request(
-      {
-        url: `https://api.darksky.net/forecast/3f19042ada29f7091ba32e63eb311699/${
-          results.geometry.lat
-        },${results.geometry.lng}`,
-        json: true
-      },
-      (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          console.log(
-            `${weather.fahrenheitToCelsius(body.currently.temperature)}°C`
-          );
+    weather.getWeather(
+      results.geometry.lat,
+      results.geometry.lng,
+      (errorMessage, results) => {
+        if (errorMessage) {
+          console.log(errorMessage);
         }else {
-          console.log("Unable to fetch the weather");
+          console.log(JSON.stringify(results));
         }
       }
     );
+
     console.log(JSON.stringify(results, undefined, 2));
   }
 });
--- weather/weather.js
+++ weather/weather.js
@@ -1,5 +1,32 @@
+const request = require('request');
+const fs = require('fs');
+
 function fahrenheitToCelsius(temperature) {
     return (temperature - 32) * (5/9);
 }
 
+function getWeather(lat, lng, callback) {
+    request(
+        {
+          url: `https://api.darksky.net/forecast/3f19042ada29f7091ba32e63eb311699/${lat},${lng}`,
+          json: true
+        },
+        (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            let currently = response.body.currently;
+            let responseObj = {
+                temperature: currently.temperature,
+                temperatureCelsius: fahrenheitToCelsius(currently.temperature),
+                humidity: currently.humidity,
+                precipProbability: currently.precipProbability
+            };
+            callback(undefined, responseObj);
+          }else {
+            callback("Unable to fetch the weather", undefined);
+          }
+        }
+      );
+}
+
+module.exports.getWeather = getWeather;
 module.exports.fahrenheitToCelsius = fahrenheitToCelsius;
